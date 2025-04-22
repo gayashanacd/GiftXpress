bd253c44ee57de7523fc75efd3b5e2af2efb0515
Sujeewa Fernando <fernandow3@student.douglascollege.ca>
2025-04-22 13:17:23 -0700

    Order feature changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
                     <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
                     <a class="nav-link active" href="{{ route('home.about') }}">About</a>
                     <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
+                    <a class="nav-link active" href="{{ route('orders.index') }}">Orders</a>
                     <div class="vr bg-white mx-2 d-none d-lg-block"></div>
                       @guest
                       <a class="nav-link active" href="{{ route('login') }}">Login</a>
